4f88420c232fce7b2abe66829730b860b804fdc5
Mohamed A. Bamakhrama <mohamed@users.noreply.github.com>
2019-01-31 11:08:39 +0100

    Working version with tmux 2.3

--- setup_tmux_terminals.sh
+++ setup_tmux_terminals.sh
@@ -2,31 +2,31 @@
 
 set -eu
 
-session=bbtnr
-device=device
-system=system
-tests=tests
+session=whc
+whc_src=src
+whc_build=build
+whc_tests=tests
+BOOST_ROOT=/hpc/shared/eb_apps/rhel6/boost/1.60.0-foss-2016a-Python-2.7.11
 
-setup_cmd="cd ~/master_ipu_models/icg-simulator-trunk && source set_project_env.sh -w IP_ipu_models -m ipu_models -p_sim ipu6_a0"
-sleep_time=15s
+
+proj_dir="/pscratch/$USER/repo/motk"
+setup_cmd="cd /pscratch/$USER/repo/motk && source $HOME/source_motk_modules.sh && source env-motk -r -c symbols -B $BOOST_ROOT && export PATH=$HOME/bin:$PATH && export LD_LIBRARY_PATH=$HOME/lib:$HOME/lib64:     $LD_LIBRARY_PATH"
+sleep_time=2s
 
 tmux new -s ${session} -d
 echo "${session} session created"
 
-cmd="cd subIP/bb_ff/source/hive/devices/bbtnr/hss"
-tmux new-window -n ${device} "${setup_cmd}"
+tmux new-window -c ${proj_dir} -t ${session} -n "${whc_src}"
 sleep ${sleep_time}
-tmux send-keys -t ${device} "${cmd}" Enter
-echo "${device} window created"
+tmux send-keys -t ${whc_src} "${setup_cmd}" Enter
+echo "${session}:${whc_src} window created"
 
-cmd="cd subIP/bb_ff/source/hive/systems/ipu6/bbtnr_test_system"
-tmux new-window -n ${system} "${setup_cmd}"
+tmux new-window -c ${proj_dir} -t ${session} -n "${whc_build}"
 sleep ${sleep_time}
-tmux send-keys -t ${system} "${cmd}" Enter
-echo "${system} window created"
+tmux send-keys -t ${whc_build} "${setup_cmd}" Enter
+echo "${session}:${whc_build} window created"
 
-cmd="cd subIP/bb_ff/source/hive/systems/ipu6/bbtnr_test_system/tests"
-tmux new-window -n ${tests} "${setup_cmd}"
+tmux new-window -c ${proj_dir} -t ${session} -n "${whc_tests}"
 sleep ${sleep_time}
-tmux send-keys -t ${tests} "${cmd}" Enter
-echo "${tests} window created"
+tmux send-keys -t ${whc_tests} "${setup_cmd}" Enter
+echo "${session}:${whc_tests} window created"
