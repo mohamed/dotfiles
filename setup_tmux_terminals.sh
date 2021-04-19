2a92ace1406f21688f8fe0ff6bfe1822093daf5a
Mohamed A. Bamakhrama <mohamed@alumni.tum.de>
2021-04-19 10:32:56 +0200

    Update setup_tmux_terminals.sh

--- setup_tmux_terminals.sh
+++ setup_tmux_terminals.sh
@@ -2,24 +2,32 @@
 
 set -eu
 
-session=proj
-src=src
+session=ev
+editor=editor
 build=build
 tests=tests
 
-proj_dir="$HOME/develop/tools/proj"
+proj_dir="/SCRATCH/mohamed/proj"
 sleep_time=2s
 
 tmux new -s ${session} -d
 echo "${session} session created"
 
-tmux rename-window -t ${session}:0 "${src}"
-tmux send-keys -t ${session}:${src} "cd ${proj_dir}/src" Enter
+tmux rename-window -t ${session}:0 "${editor}"
+tmux send-keys -t ${session}:${editor} "cd ${proj_dir}" Enter
 
 tmux new-window -P -c ${proj_dir} -t ${session}:1 -n "${build}"
-tmux send-keys -t ${session}:${build} "cd ${HOME}/build" Enter
+tmux send-keys -t ${session}:${build} "cd ${proj_dir}" Enter
+tmux send-keys -t ${session}:${build} "source setup.sh" Enter
+sleep ${sleep_time}
+
+tmux split-window -h -c ${proj_dir} -t ${session}:${build}
+tmux send-keys -t ${session}:${build}.1 "cd ${proj_dir}" Enter
+tmux send-keys -t ${session}:${build}.1 "source setup.sh" Enter
+sleep ${sleep_time}
 
 tmux new-window -P -c ${proj_dir} -t ${session}:2 -n "${tests}"
-tmux send-keys -t ${session}:${tests} "cd tests" Enter
+tmux send-keys -t ${session}:${tests} "cd ${proj_dir}" Enter
+tmux send-keys -t ${session}:${tests} "source setup.sh" Enter
 
 echo "All done"
