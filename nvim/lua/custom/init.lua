61ee688b49b5f21cd40bec44629dd7eec1bc6a72
Mohamed A. Bamakhrama <mohamed@alumni.tum.de>
2022-04-21 14:21:13 +0200

    Update init.lua

--- nvim/lua/custom/init.lua
+++ nvim/lua/custom/init.lua
@@ -2,3 +2,6 @@ require'lspconfig'.clangd.setup{}
 require'lspconfig'.pyright.setup{}
 require'lspconfig'.bashls.setup{}
 require'lspconfig'.svls.setup{}
+
+local map = require("core.utils").map
+map("n", "<leader>fr", ":Files <CR>")
